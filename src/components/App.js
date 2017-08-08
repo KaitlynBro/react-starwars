4061da2d5fb4e8fa9874391c4f65f376ad1e8241
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2017-08-08 19:21:42 -0400

    final commits

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+//importing React, React Router, and all my components
 import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 import Header from './Header';
@@ -5,25 +6,30 @@ import Search from './Search';
 import Results from './Results';
 import Details from './Details';
 
+//main component where all of my smaller components will live
 class App extends Component {
   constructor() {
     super();
+    //this is the default state of the search results. it is empty when the page loads
+      //therefore, the default state is just an empty array
     this.state = {
       searchResults: [],
     };
   }
-
+  //creating function that holds the recieved query results in an object
+    //setting default state to the results function
   receiveResults(data) {
     this.setState({ searchResults: data });
   }
 
+
   render() {
     return (
       <div className="App">
-        {/* Bind this.receiveResults to the App component and pass it as a
-          prop to the Search component */}
+        {/*implementing Browser Router into App component that will handle requests*/}
         <BrowserRouter>
           <Switch>
+            {/*applying Route to watch for a query string that will match the /react-starwars pathname and when there is a match, render component*/}
             <Route exact path="/" render={() => <Redirect to="/react-starwars" />} />
             <Route
               exact
@@ -31,6 +37,7 @@ class App extends Component {
               render={() =>
                 (<div>
                   <Header />
+                  {/*binding this.recieveResults to the App component and passing it as a prop to the search component*/}
                   <Search receiveResults={data => this.receiveResults(data)} />
                   {this.state.searchResults.length
                     ? <Results results={this.state.searchResults} />
